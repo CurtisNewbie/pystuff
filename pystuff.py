d982bcda96977c8bf60ba8ccdd67077de53fb00c
yongj.zhuang <yongj.zhuang@outlook.com>
2023-07-06 15:11:49 +0800

    print_table(), space_tokenize()

--- pystuff.py
+++ pystuff.py
@@ -1,5 +1,6 @@
 from datetime import *
 import random
+import shlex
 import string
 import unicodedata
 
@@ -18,6 +19,7 @@ def spaces(cnt: int) -> str:
     '''
     Generate N spaces characters joined as string
     '''
+    if cnt < 1: return ""
     return gen_tokens(cnt, " ")
 
 
@@ -25,6 +27,7 @@ def str_width(s: str) -> int:
     '''
     Calculate the actual rendered width of string (i.e., actual width of half/full width characters)
     '''
+    if not s: return 0
     l = 0
     for i in range(len(s)):
         w = unicodedata.east_asian_width(s[i])
@@ -75,3 +78,38 @@ def escape_quote(s: str, quote: str = "'", escape: str = '\\') -> str:
     Escape quote with \\ prefix
     '''
     return s.replace(quote, f'{escape}{quote}')
+
+
+def space_tokenize(s: str) -> list[str]:
+    '''
+    Split string into tokens, spaces in quoted string are preserved
+    '''
+    return shlex.split(s, posix=False)
+
+
+def print_table(col: list[str], rows: list[list[str]]):
+    # max length among the rows
+    indent : dict[int][int] = {}
+    for i in range(len(col)): indent[i] = str_width(col[i])
+    for r in rows:
+        for i in range(len(col)): indent[i] = max(indent[i], str_width(r[i]))
+
+    print()
+    col_title = "| "
+    col_sep = "|-"
+    for i in range(len(col)):
+        if col[i] is None: col[i] = ''
+        col_title += col[i] + spaces(indent[i] - str_width(col[i]) + 1) + " | "
+        col_sep += gen_tokens(indent[i] + 1, "-") + "-|"
+        if i < len(col) - 1: col_sep += "-"
+    print(col_sep + "\n" + col_title + "\n" + col_sep)
+
+    for r in rows:
+        row_ctn = "| "
+
+        for i in range(len(col)):
+            rv = r[i]
+            if rv is None: rv = ''
+            row_ctn += rv + spaces(1 + indent[i] - str_width(rv)) + " | "
+        print(row_ctn)
+    print(col_sep)
