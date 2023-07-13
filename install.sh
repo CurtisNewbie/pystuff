030f0b59cc5d42f5de114c03df9cf206470dd8e9
yongj.zhuang <yongj.zhuang@outlook.com>
2023-07-13 10:10:41 +0800

    Modified:  install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 CURR=$(pwd) \
 	&& echo "Installing pystuff, previous working directory: $CURR" && echo \
-	&& cd /tmp && git clone https://github.com/CurtisNewbie/pystuff && cd pystuff \
+	&& cd /tmp \
+	&& echo "Cloing pystuff to /tmp" && echo \
+	&& git clone https://github.com/CurtisNewbie/pystuff && cd pystuff \
 	&& python3 -m pip install . \
 	&& rm -rf pystuff \
 	&& cd $CURR
