90c74776efd77006cb25d700bf70de7180440648
yongj.zhuang <yongj.zhuang@outlook.com>
2023-07-13 10:14:48 +0800

    Modified:  install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+git_repo="https://github.com/CurtisNewbie/pystuff"
+app="pystuff"
+
 CURR=$(pwd) \
-	&& echo "Installing pystuff, previous working directory: $CURR" && echo \
+	&& echo "Using $(python3 --version)" \
+	&& echo "Installing $app, previous working directory: $CURR" \
 	&& cd /tmp \
-	&& echo "Cloing pystuff to /tmp" && echo \
-	&& git clone https://github.com/CurtisNewbie/pystuff --depth 1 && python3 -m pip install pystuff \
-	&& rm -rf pystuff \
-	&& echo "/tmp/pystuff removed" \
-	&& cd $CURR
+	&& echo "Cloing $app to /tmp" \
+	&& git clone "$git_repo" --depth 1 && (cd "$app" && python3 -m pip install .) \
+	&& rm -rf "$app" \
+	&& echo "/tmp/$app removed" \
+	&& cd "$CURR"
