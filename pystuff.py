3d21ecf00d7b45d8ddbce3958e874f630f31397a
yongj.zhuang <yongj.zhuang@outlook.com>
2023-09-25 16:06:34 +0800

    Add walk() method

--- pystuff.py
+++ pystuff.py
@@ -6,7 +6,7 @@ import unicodedata
 import sys
 import subprocess
 import os
-import readline # don't remove this, this is for input()
+import readline  # don't remove this, this is for input()
 
 T = '    '  # four space tab
 TT = T + T  # two tabs
@@ -66,7 +66,8 @@ def spaces(cnt: int) -> str:
     '''
     Generate N spaces characters joined as string
     '''
-    if cnt < 1: return ""
+    if cnt < 1:
+        return ""
     return gen_tokens(cnt, " ")
 
 
@@ -74,7 +75,8 @@ def str_width(s: str) -> int:
     '''
     Calculate the actual rendered width of string (i.e., actual width of half/full width characters)
     '''
-    if not s: return 0
+    if not s:
+        return 0
     l = 0
     for i in range(len(s)):
         w = unicodedata.east_asian_width(s[i])
@@ -134,7 +136,7 @@ def space_tokenize(s: str) -> list[str]:
     return shlex.split(s, posix=False)
 
 
-def index_list(ar : list[str]) -> dict[str]:
+def index_list(ar: list[str]) -> dict[str]:
     '''
     Build index for the values in list, returns a dict where K is the value, V is the index
     '''
@@ -152,30 +154,38 @@ def print_table(col: list[str], rows: list[list[str]], include_line_end: bool = 
     printed = []
 
     # max length among the rows
-    indent : dict[int][int] = {}
-    for i in range(len(col)): indent[i] = width(col[i])
+    indent: dict[int][int] = {}
+    for i in range(len(col)):
+        indent[i] = width(col[i])
     for r in rows:
-        for i in range(len(col)): indent[i] = max(indent[i], width(r[i]))
+        for i in range(len(col)):
+            indent[i] = max(indent[i], width(r[i]))
 
     col_title = "| "
     col_sep = "|-"
     for i in range(len(col)):
-        if col[i] is None: col[i] = ''
-        if exclude_cols and col[i] in exclude_cols: continue
+        if col[i] is None:
+            col[i] = ''
+        if exclude_cols and col[i] in exclude_cols:
+            continue
         col_title += col[i] + spaces(indent[i] - width(col[i]) + 1) + " | "
         col_sep += gen_tokens(indent[i] + 1, "-") + "-|"
-        if i < len(col) - 1: col_sep += "-"
+        if i < len(col) - 1:
+            col_sep += "-"
     printed.append(col_sep + "\n" + col_title + "\n" + col_sep)
 
     for r in rows:
         row_ctn = "| "
 
         for i in range(len(col)):
-            if exclude_cols and col[i] in exclude_cols: continue
+            if exclude_cols and col[i] in exclude_cols:
+                continue
             rv = r[i]
-            if rv is None: rv = ''
+            if rv is None:
+                rv = ''
             line = rv + spaces(1 + indent[i] - width(rv))
-            if i < len(col) - 1 or include_line_end: line += " | "
+            if i < len(col) - 1 or include_line_end:
+                line += " | "
             row_ctn += line
         printed.append(row_ctn)
     printed.append(col_sep)
@@ -206,7 +216,7 @@ def default_open(filename):
     if platform == 'darwin':
         subprocess.call(('open', filename))
     elif platform in ['win64', 'win32']:
-        os.startfile(filename.replace('/','\\'))
+        os.startfile(filename.replace('/', '\\'))
     elif platform == 'wsl':
         subprocess.call('cmd.exe /C start'.split() + [filename])
     else:                                   # linux variants
@@ -218,7 +228,8 @@ def quote_list(l: list[str], quote: str = "'"):
     Quote each item in the list with a single quote (by default), a new list is returned
     '''
     j = []
-    for i in range(len(l)): j.append(f"{quote}{l[i]}{quote}")
+    for i in range(len(l)):
+        j.append(f"{quote}{l[i]}{quote}")
     return j
 
 
@@ -228,7 +239,8 @@ def rev_idx(ele: list[str], val: set[str] = None) -> dict[str]:
     '''
     idx = {}
     for i in range(len(ele)):
-        if val is None or ele[i] in val: idx[ele[i]]  = i
+        if val is None or ele[i] in val:
+            idx[ele[i]] = i
     return idx
 
 
@@ -238,7 +250,8 @@ def filter_by_idx(l: list[str], idx: set[int]) -> list[str]:
     '''
     r = []
     for i in range(len(l)):
-        if i in idx: continue
+        if i in idx:
+            continue
         r.append(l[i])
     return r
 
@@ -260,10 +273,13 @@ def completer(text, state):
     Internally, it uses a global variable named completer_candidates, which is basically a set of string
     '''
     global completer_candidates
-    if not text: return None
+    if not text:
+        return None
     options = [cmd for cmd in completer_candidates if cmd.startswith(text)]
-    if state < len(options): return options[state]
-    else: return None
+    if state < len(options):
+        return options[state]
+    else:
+        return None
 
 
 def feed_completer_nested(rl: list[list]):
@@ -271,7 +287,8 @@ def feed_completer_nested(rl: list[list]):
     feed words to completer
     '''
     for r in rl:
-        for v in r: feed_completer_nested(v)
+        for v in r:
+            feed_completer_nested(v)
 
 
 def feed_completer(word: str):
@@ -279,23 +296,37 @@ def feed_completer(word: str):
     feed words to completer
     '''
     global completer_candidates
-    if not word: return
+    if not word:
+        return
     completer_candidates.add(word)
 
 
-def dict_get(d, expr: str, default_val = None) -> any:
+def walk(dic, *nested_fields: str) -> any:
+    '''
+    Walk the dictionary, return None if not found.
+    '''
+    return dict_get(dic, '.'.join(nested_fields))
+
+
+def dict_get(d, expr: str, default_val=None) -> any:
     '''
     Retrieve value from a dict recursively by providing a dot delimited expression, e.g., name.key.juice
     '''
-    if not expr or not d: return default_val
-    if type(d) != dict: return default_val
+    if not expr or not d:
+        return default_val
+    if type(d) != dict:
+        return default_val
     sp = expr.split(".", 1)
 
-    if not sp[0] in d: d = None
-    else: d = d[sp[0]]
+    if not sp[0] in d:
+        d = None
+    else:
+        d = d[sp[0]]
 
-    if len(sp) > 1: return dict_get(d, sp[1], default_val)
-    else: return d if d else default_val
+    if len(sp) > 1:
+        return dict_get(d, sp[1], default_val)
+    else:
+        return d if d else default_val
 
 
 def str_matches(t: str, v: str) -> bool:
@@ -307,7 +338,5 @@ class DictWalker():
     def __init__(self, target: dict):
         self.target = target
 
-    def get(self, expr: str, default_val = None) -> any:
+    def get(self, expr: str, default_val=None) -> any:
         return dict_get(self.target, expr, default_val)
-
-
